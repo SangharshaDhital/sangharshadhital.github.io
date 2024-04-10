095961dda93bb555ba821371c2ecb242666569aa
SangharshaDhital <dhitalsangharsha@gmail.com>
2024-04-09 22:53:40 -0700

    title commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Writer", "Thinker","Photographer", "Philoshoper"],
+        strings: ["Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer","Thinker","Analyser"],
+        strings: ["Developer","Thinker","Learner"],
         typeSpeed: 100,
         backSpeed: 55,
         loop: true
